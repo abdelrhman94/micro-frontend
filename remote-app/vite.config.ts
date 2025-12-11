74b1de188f0b4e0058be5ab57c4c66bfaeb135f7
Abdelrhman Farag <abdelrhman.s.farag@gmail.com>
2025-12-11 14:18:58 +0300

    intial micro frontend repo

---
+++ remote-app/vite.config.ts
@@ -0,0 +1,30 @@
+import federation from '@originjs/vite-plugin-federation';
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    federation({
+      name: 'remote-app',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './Button': './src/components/Button',
+        './Header': './src/components/Header',
+      },
+      shared: ['react', 'react-dom'],
+    }),
+  ],
+  build: {
+    modulePreload: false,
+    target: 'esnext',
+    minify: false,
+    cssCodeSplit: false,
+  },
+  preview: {
+    port: 5001,
+    strictPort: true,
+    cors: true,
+  },
+});
